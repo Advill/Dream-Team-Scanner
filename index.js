0e97a978de19c42ded7b414318b314aa91203920
Advill <mcgloinms@gmail.com>
2019-07-24 16:34:35 -0400

    Start server work

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const parser = require('./receipt-parser.js');
+const express = require('express');
+const app = express();
 
-parser.parseReceipt('./examples/receipt.jpg').then(function(attribs) {
-  console.log(attribs);
+app.get('/', function(req, res) {
+  val = parse()
+  res.status(200).send('attribs');
 });
+
+if (module === require.main) {
+  // [START server]
+  const server = app.listen(process.env.PORT || 8080, () =>
+    const port = server.aaddress().port;
+    console.log('App listening on port ${port}');
+  );
+}
+
+module.exports = app;
+
+function parse() {
+  parser.parseReceipt('./examples/receipt.jpg').then(function(attribs) {
+    console.log(attribs);
+  });
+}
 
