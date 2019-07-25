316f42fc9c968c81b458ca1d965fbe6e4b79b44c
Advill <mcgloinms@gmail.com>
2019-07-25 13:14:20 -0400

    Begin serving static pages

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 
 app.get('/', function(req, res) {
+  res.redirect('/index.html');
+});
+app.get('/test', function(req, res) {
   parse('./examples/receipt.jpg').then(function(attribs) {
     console.log(attribs);
     res.status(200).json(attribs);
@@ -18,12 +21,19 @@ app.post('/upload', upload.single('Image'), function (req, res, next) {
   }).catch(err => res.status(418).send("busted"));
 });
 
+app.use(express.static('app'));
+
 if (module === require.main) {
   const server = app.listen(process.env.PORT || 8080, () => {
     const port = server.address().port;
     console.log('App listening on port ${port}');
   });
 }
+
+// redirect all else to index
+app.get('*', function (req, res) {
+  res.redirect('/index.html');
+});
 
 module.exports = app;
 
