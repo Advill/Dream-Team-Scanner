b1bf79e946841ba86090de028cd73c28ea80fa53
Advill <mcgloinms@gmail.com>
2019-07-25 10:19:07 -0400

    Add functionality to receive image

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const parser = require('./receipt-parser.js');
 const express = require('express');
 const app = express();
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 app.get('/', function(req, res) {
   parse().then(function(attribs) {
     console.log(attribs);
     res.status(200).json(attribs);
   });
+});
+
+app.post('/upload', upload.single('Image'), function (req, res, next) {
+  console.log(req.file.path);
+  res.status(200).json({test: "ok"});
 });
 
 if (module === require.main) {
